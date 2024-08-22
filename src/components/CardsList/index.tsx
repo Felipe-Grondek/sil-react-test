88039483ef68d783133c8f78d6d4ce758e7e1ed2
Felipe Grondek <felipegrondek00@gmail.com>
2024-08-21 22:53:12 -0300

    feat: add cards grid

---
+++ src/components/CardsList/index.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+import { Grid } from '@chakra-ui/react';
+
+interface CardsListProps {
+  children: ReactNode
+}
+
+export function CardsList({ children }: CardsListProps) {
+  return (
+    <Grid
+      templateColumns={{
+        base: 'repeat(1, 1fr)',
+        md: 'repeat(2, 1fr)',
+        lg: 'repeat(3, 1fr)'
+      }}
+      gap='5'
+    >
+      {children}
+    </Grid>
+  )
+}
