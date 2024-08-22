6fcf175135c88648e7e6e26214f3440fa6f545c2
Felipe Grondek <felipegrondek00@gmail.com>
2024-08-21 22:51:10 -0300

    styles: add core app colors

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      300: '#63b3ed',
+      900: '#1A365D'
+    },
+    secondary: {
+      500: '#02b57a'
+    }
+  }
+})
