a1730a00b8def9fb4be52af04c3d2dc58ab76ad9
Felipe Grondek <felipegrondek00@gmail.com>
2024-08-25 12:58:42 -0300

    deploy: update connection with db

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,13 +1,13 @@
 import axiosClass from 'axios'
 
 // Use to deployed database
-// export const deployURL = 'https://sil-cards-test-database.vercel.app'
+export const deployURL = 'https://sil-react-test-db.vercel.app/'
 
 // Use to local requests
 export const localURL = 'http://localhost:3000'
 
 export const axios = axiosClass.create({
-  baseURL: localURL,
+  baseURL: deployURL,
   timeout: 5000,
   headers: { 'Content-Type': 'application/json' }
 })
